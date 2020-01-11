f34673449c13d04e1f08e4443881275f39a74494
Ben Bastedo <bbastedo@comcast.net>
2020-01-11 11:26:05 -0600

    Added new pages for individual stock graphs

--- app.py
+++ app.py
@@ -38,5 +38,25 @@ def s4():
 def s5():
     return render_template("Season_5.html")
 
+@app.route("/S1")
+def stock1():
+    return render_template("s1.html")    
+
+@app.route("/S2")
+def stock2():
+    return render_template("s2.html")    
+
+@app.route("/S3")
+def stock3():
+    return render_template("s3.html")    
+
+@app.route("/S4")
+def stock4():
+    return render_template("s4.html")    
+
+@app.route("/S5")
+def stock5():
+    return render_template("s5.html")    
+
 if __name__ == "__main__":
     app.run(debug=True)
